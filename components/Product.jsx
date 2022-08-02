3c7df36b4522c413844842ea6d4673570a1aa505
Dung-Dinh <dungvanbmt.01@gmail.com>
2022-08-02 16:19:03 +0700

    take data from sanity add  in component

--- components/Product.jsx
+++ components/Product.jsx
@@ -9,15 +9,16 @@ const Product = ({product:{
 }}) => {
   return (
   <div>
-    <Link href={`/product${slug.current}`}>
+    <Link href={`/product/${slug.current}`}>
       <div className="product-card">
-        <img src={urlFor(image && image[0])} 
+        <img 
+        src={urlFor(image && image[0])} 
         width={250}
         height={250}
-        className="product_image"
+        className="product-image"
          />
          <p className="product-name">{name}</p>
-         <p className="product-price">{price}</p>
+         <p className="product-price">${price}</p>
       </div>
    </Link>
   </div>
